fe1dc467b9bd72f4cdff7bcca3472b8a0561d607
Taranenko MBP2 <olegtaranenko@gmail.com>
2019-03-07 02:51:36 +0100

    first mockito test

---
+++ src/test/java/com/olegtaranenko/udemy/recipe/services/RecipeServiceImplTest.java
@@ -0,0 +1,47 @@
+package com.olegtaranenko.udemy.recipe.services;
+
+import com.olegtaranenko.udemy.recipe.domain.Recipe;
+import com.olegtaranenko.udemy.recipe.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+public class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() {
+        Recipe recipe = new Recipe();
+
+        HashSet recipesData = new HashSet();
+
+        recipesData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(recipes.size(), 1);
+
+        Mockito.verify(recipeRepository, Mockito.times(1)).findAll();
+    }
+}
