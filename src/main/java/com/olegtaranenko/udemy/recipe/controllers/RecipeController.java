3331d77d7c16ee2582cc52d8eb295b06e8653989
Oleg Taranenko <olegtaranenko@gmail.com>
2019-03-09 18:05:59 +0100

    new recipe

--- src/main/java/com/olegtaranenko/udemy/recipe/controllers/RecipeController.java
+++ src/main/java/com/olegtaranenko/udemy/recipe/controllers/RecipeController.java
@@ -1,10 +1,14 @@
 package com.olegtaranenko.udemy.recipe.controllers;
 
+import com.olegtaranenko.udemy.recipe.commands.RecipeCommand;
+import com.olegtaranenko.udemy.recipe.domain.Recipe;
 import com.olegtaranenko.udemy.recipe.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import static java.lang.Long.parseLong;
@@ -27,5 +31,20 @@ public class RecipeController {
         model.addAttribute("recipe", recipeService.findById(parseLong(id)));
 
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand saveeCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show" + saveeCommand.getId();
     }
 }
